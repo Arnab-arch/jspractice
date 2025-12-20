d83512b3a9c660c211a0a475b9fd726fe9386b45
Arnab Sheoran <144190865+Akash-Sheoran@users.noreply.github.com>
2025-12-21 02:03:45 +0530

    initial learning

---
+++ jspractice.js
@@ -0,0 +1,28 @@
+/*basic function in js :
+     functions are the main building block of a program they allow to call a single code many times 
+     without repeatation 
+
+     declaration :
+
+     function-keyword  function-name (parameters){
+     // body
+     }
+     to call a function simply write function name 
+    
+*/
+function showmessage(){
+  console.log("hello all");  
+}
+showmessage()
+
+
+
+
+
+
+
+
+
+
+
+ 
